ea20bb4a3c53ae55b294d691bf7a2c6bda92ea93
biscutii <isabelleyparkes@gmail.com>
2025-01-05 20:45:59 +0000

    Board is printing

--- code.c
+++ code.c
@@ -10,14 +10,57 @@ struct sudokuBoard createSudokuBoard(struct sudokuBoard s)// There will be 30 fi
     // If chosen location is already filled, reroll x and y coordinate
     // Roll random number from 0 to 9
     // If chosen number is already placed vertically or horizontally on the same lines then reroll
-    
-    int x = rand();
-    int y = rand();
 
-    if (s.board[y][x] == 0)
+    int cell_min_number = 1;
+    int cell_max_number = 9;
+    int min_number = 0;
+    int max_number = 8;
+    int filled_cells = 0;
+
+    while (filled_cells != 30) // While filled cells is not equal to 30
     {
-        printf("%s", "ran");
+
+        int x = rand() % (max_number + 1 - min_number) + min_number;
+        int y = rand() % (max_number + 1 - min_number) + min_number;
+
+        if (s.board[y][x] == 0) // If randomly selected cell is empty
+        {
+
+            int filling_cell = 1;
+
+            while (filling_cell)
+            {
+
+                int is_number_colliding = 0;
+                int cell_number = rand() % (cell_max_number + 1 - cell_min_number) + cell_min_number;
+                int i;
+
+                for (i = 0; i < 9; i++)
+                {
+                    if (s.board[y][i] == cell_number)
+                    {
+                        is_number_colliding = 1;
+                    }
+                }
+
+                for (i = 0; i < 9; i++)
+                {
+                    if (s.board[i][x] == cell_number)
+                    {
+                        is_number_colliding = 1;
+                    }
+                }
+
+                if (is_number_colliding == 0)
+                {
+                    s.board[y][x] = cell_number;
+                    ++filled_cells;
+                    filling_cell = 0;
+                }
+            }
+        }
     }
+
 
     return s;
 }
@@ -26,6 +69,29 @@ int main ()
 {
     srand(time(NULL));
     struct sudokuBoard s;
+    int x;
+    int y;
+
+    for (y = 0; y < 9; y++)
+    {
+        for (x = 0; x < 9; x++)
+        {
+            s.board[y][x] = 0;
+        }
+    }
+
     s = createSudokuBoard(s);
+
+    for (y = 0; y < 9; y++)
+    {
+        for (x = 0; x < 9; x++)
+        {
+            printf("%d", s.board[y][x]);
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
 
